81471f2795415a41396d4f8f831d994d085a68e9
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2025-04-29 20:00:58 +0530

    url path updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// export const BASE_URL="https://github-livid-phi.vercel.app"
-export const BASE_URL="http://localhost:808"
+export const BASE_URL="https://github-livid-phi.vercel.app"
+// export const BASE_URL="http://localhost:808"
 const token=localStorage.getItem("jwt")
 
 export const api= axios.create({
